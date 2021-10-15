5dfa6ce6e7c7226a4b627cd8be305987d33c4a94
Vilius Gečas <gecas97@gmail.com>
2021-10-15 15:52:29 +0300

    Add IBookRepository

---
+++ VismaTask/Repositories/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using VismaTask.Models;
+
+namespace VismaTask.Repositories
+{
+    public interface IBookRepository
+    {
+        public Book AddBook(Book book);
+        public TakenBook AddTakenBook(TakenBook takenBook);
+        public List<Book> GetAllBooksData();
+        public List<TakenBook> GetAllTakenBooksData();
+        public void DeleteBookFromBooksDataFile(Book book);
+        public void DeleteBookFromTakenBooksDataFile(TakenBook takenBook);
+        public int GetUserTakenBookAmmount(string userId);
+        public Book GetBook(string ISBN);
+        public TakenBook GetTakenBook(string userId, string ISBN);
+    }
+}
