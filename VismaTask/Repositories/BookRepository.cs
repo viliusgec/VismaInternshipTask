5967cb343e13cf5110ce82f3f74fa302d45ed62d
Vilius Gečas <gecas97@gmail.com>
2021-10-15 15:52:59 +0300

    Add Deserialize method

--- VismaTask/Repositories/BookRepository.cs
+++ VismaTask/Repositories/BookRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 using VismaTask.Models;
+using VismaTask.Repositories;
 
 namespace VismaTask.Repository
 {
-    public class BookRepository
+    public class BookRepository : IBookRepository
     {
         private static readonly string BooksDataFile = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + @"\Data\BookData.json";
         private static readonly string TakenBooksDataFile = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + @"\Data\TakenBooksData.json";
@@ -16,35 +17,37 @@ namespace VismaTask.Repository
 
         }
 
-        public void AddBook(Book book)
+        public Book AddBook(Book book)
         {
-            List<Book> books = JsonSerializer.Deserialize<List<Book>>(File.ReadAllText(BooksDataFile));
+            List<Book> books = DeserializeBooks();
             books.Add(book);
             string newJsonString = JsonSerializer.Serialize(books);
             File.WriteAllText(BooksDataFile, newJsonString);
+            return book;
         }
 
-        public void AddTakenBook(TakenBook takenBook)
+        public TakenBook AddTakenBook(TakenBook takenBook)
         {
             List<TakenBook> books = JsonSerializer.Deserialize<List<TakenBook>>(File.ReadAllText(TakenBooksDataFile));
             books.Add(takenBook);
             string newJsonString = JsonSerializer.Serialize(books);
             File.WriteAllText(TakenBooksDataFile, newJsonString);
+            return takenBook;
         }
 
         public List<Book> GetAllBooksData()
         {
-            return JsonSerializer.Deserialize<List<Book>>(File.ReadAllText(BooksDataFile));
+            return DeserializeBooks();
         }
 
         public List<TakenBook> GetAllTakenBooksData()
         {
-            return JsonSerializer.Deserialize<List<TakenBook>>(File.ReadAllText(TakenBooksDataFile));
+            return DeserializeTakenBooks();
         }
 
         public void DeleteBookFromBooksDataFile(Book book)
         {
-            List<Book> books = JsonSerializer.Deserialize<List<Book>>(File.ReadAllText(BooksDataFile));
+            List<Book> books = DeserializeBooks();
             books.RemoveAll(x => x.ISBN == book.ISBN);
             string newJsonString = JsonSerializer.Serialize(books);
             File.WriteAllText(BooksDataFile, newJsonString);
@@ -52,7 +55,7 @@ namespace VismaTask.Repository
 
         public void DeleteBookFromTakenBooksDataFile(TakenBook takenBook)
         {
-            List<TakenBook> takenBooks = JsonSerializer.Deserialize<List<TakenBook>>(File.ReadAllText(TakenBooksDataFile));
+            List<TakenBook> takenBooks = DeserializeTakenBooks();
             takenBooks.Remove(takenBook);
             string newJsonString = JsonSerializer.Serialize(takenBooks);
             File.WriteAllText(TakenBooksDataFile, newJsonString);
@@ -60,20 +63,30 @@ namespace VismaTask.Repository
 
         public int GetUserTakenBookAmmount(string userId)
         {
-            List<TakenBook> takenBooks = JsonSerializer.Deserialize<List<TakenBook>>(File.ReadAllText(TakenBooksDataFile));
+            List<TakenBook> takenBooks = DeserializeTakenBooks();
             return takenBooks.FindAll(x => x.UserId == userId).Count;
         }
 
         public Book GetBook(string ISBN)
         {
-            List<Book> books = JsonSerializer.Deserialize<List<Book>>(File.ReadAllText(BooksDataFile));
+            List<Book> books = DeserializeBooks();
             return books.Find(x => x.ISBN == ISBN);
         }
 
         public TakenBook GetTakenBook(string userId, string ISBN)
         {
-            List<TakenBook> books = JsonSerializer.Deserialize<List<TakenBook>>(File.ReadAllText(TakenBooksDataFile));
+            List<TakenBook> books = DeserializeTakenBooks();
             return books.Find(x => x.Book.ISBN == ISBN && x.UserId == userId);
+        }
+
+        private List<Book> DeserializeBooks()
+        {
+            return File.Exists(BooksDataFile) ? JsonSerializer.Deserialize<List<Book>>(File.ReadAllText(BooksDataFile)) : new List<Book>();
+        }
+
+        private List<TakenBook> DeserializeTakenBooks()
+        {
+            return File.Exists(TakenBooksDataFile) ? JsonSerializer.Deserialize<List<TakenBook>>(File.ReadAllText(TakenBooksDataFile)) : new List<TakenBook>();
         }
     }
 }
